a4c8f863d13d636f2450a32b7fe1c5d9b377ef87
Uttam patel <utampipaliya@gmail.com>
2022-11-14 19:16:24 +0530

    code

---
+++ Python Evaluation.py
@@ -0,0 +1,13 @@
+# eval() helps in evaluating an expression. The expression can be a Python statement, or a code object.
+#  eval() can also be used to work with Python keywords or defined functions and variables. These would normally be stored as strings.
+
+
+var = input()
+eval(var)
+
+
+# Input (stdin)
+# print(2 + 3)
+
+# Expected Output
+# 5
