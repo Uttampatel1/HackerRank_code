7e369701607ec831966f9b19076bbc86bb6c3bcc
Uttam patel <utampipaliya@gmail.com>
2022-11-19 10:04:23 +0530

    split

---
+++ Re split().py
@@ -0,0 +1,13 @@
+regex_pattern = r"\.|\,"	# Do not delete 'r'.
+
+import re
+print("\n".join(re.split(regex_pattern, input())))
+
+# Input (stdin)
+# 100,000,000.000
+
+# Expected Output
+# 100
+# 000
+# 000
+# 000
